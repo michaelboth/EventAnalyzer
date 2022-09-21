8a1e0eb0ab8e7f6d1894705bc345d11a047d0358
Michael Both <michael23.both@gmail.com>
2022-09-21 12:00:04 -0400

    Updated

--- packaging/buildWindowsPackage.bat
+++ packaging/buildWindowsPackage.bat
@@ -24,11 +24,11 @@ echo "vs_folder = %vs_folder%"
 
 rem Create needed foldeers
 mkdir %output_folder%
-if %ERRORLEVEL% neq 0 ( echo "Failed to create folder" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to create folder" & GOTO:done )
 mkdir %output_folder%\visualizer
-if %ERRORLEVEL% neq 0 ( echo "Failed to create folder" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to create folder" & GOTO:done )
 mkdir %output_folder%\visualizer\windows
-if %ERRORLEVEL% neq 0 ( echo "Failed to create folder" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to create folder" & GOTO:done )
 
 rem Copy the relevant files
 copy ..\LICENSE %output_folder%
@@ -41,13 +41,13 @@ xcopy /E/I ..\inc %output_folder%\inc
 rem Build the unikorn viewer
 cd ..\visualizer
 qmake
-if %ERRORLEVEL% neq 0 ( echo "Failed to qmake visualizer" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to qmake visualizer" & GOTO:done )
 nmake
-if %ERRORLEVEL% neq 0 ( echo "Failed to build visualizer" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to build visualizer" & GOTO:done )
 copy release\UnikornViewer.exe ..\packaging\%output_folder%\visualizer\windows
-if %ERRORLEVEL% neq 0 ( echo "Failed to copy visualizer" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to copy visualizer" & GOTO:done )
 nmake distclean
-if %ERRORLEVEL% neq 0 ( echo "Failed to clean visualizer" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to clean visualizer" & GOTO:done )
 cd ..\packaging
 
 rem Add support files for UnikornViewer.exe
@@ -55,7 +55,7 @@ rem Add support files for UnikornViewer.exe
 
 rem Compress package
 powershell Compress-Archive %output_folder% %output_folder%-win64.zip
-if %ERRORLEVEL% neq 0 ( echo "Failed to zip package" & GOTO:done )
+if ERRORLEVEL 1 ( echo "Failed to zip package" & GOTO:done )
 
 echo "Unikorn Windows package created"
 
